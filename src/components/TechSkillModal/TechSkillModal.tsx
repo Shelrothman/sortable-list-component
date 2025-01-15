74f05ac0fef608370224df0a948684c9e4eb390a
Shelby Rothman <shelby.rothman@aderant.com>
2025-01-15 09:53:15 -0800

    task: refactor modal state logic into reducers

--- src/components/TechSkillModal/TechSkillModal.tsx
+++ src/components/TechSkillModal/TechSkillModal.tsx
@@ -5,11 +5,65 @@ import CloseIcon from '@mui/icons-material/Close';
 import { IconButton } from '@mui/material';
 import { InputItem } from '@components/InputItem';
 import { DisplayItem } from '@/components/DisplayItem';
-import { Skill, SkillPosition } from '@/types';
-import { tech } from '@data/tech';
+import { Skill, SkillPosition, SkillRefObject } from '@/types';
 import { freshSkillMap } from '@utils/constants';
 
 import './techSkillModal.css';
+
+// todo: obvs clean modulate this whole file up
+// clean up/ modernize and make more efficient
+
+// todo: all reducer logic into our context file
+
+type SkillState = {
+	unselectedTechSkills: Skill[];
+	currentPosition: SkillPosition;
+};
+
+type SkillAction =
+	| { type: 'RESET'; payload: Skill[] }
+	| { type: 'ADD_SKILL'; payload: Skill }
+	| {
+			type: 'REMOVE_SKILL';
+			payload: { skill: Skill; position: SkillPosition; isFull: boolean };
+	  };
+
+const isFull = (obj: SkillRefObject) => Object.values(obj).every((s) => s.id);
+
+const initialSkillState = (fullList: Skill[]): SkillState => ({
+	unselectedTechSkills: fullList,
+	currentPosition: 1,
+});
+
+const skillReducer = (state: SkillState, action: SkillAction): SkillState => {
+	switch (action.type) {
+		case 'RESET':
+			return initialSkillState(action.payload);
+		case 'ADD_SKILL':
+			return {
+				...state,
+				unselectedTechSkills: state.unselectedTechSkills.filter(
+					(s) => s.id !== action.payload.id
+				),
+				currentPosition: (state.currentPosition >= 5
+					? state.currentPosition
+					: state.currentPosition + 1) as SkillPosition,
+			};
+		case 'REMOVE_SKILL':
+			return {
+				...state,
+				unselectedTechSkills: [
+					...state.unselectedTechSkills,
+					action.payload.skill,
+				],
+				currentPosition: action.payload.isFull
+					? 5
+					: ((state.currentPosition - 1) as SkillPosition),
+			};
+		default:
+			return state;
+	}
+};
 
 interface TechSkillModalProps {
 	show: boolean;
@@ -20,55 +74,49 @@ export const TechSkillModal: React.FC<TechSkillModalProps> = ({
 	show,
 	onClose,
 }) => {
-	const {
-		unselectedTechSkills, // todo: may be able just have this local state here and not need in the context
-		setUnselectedTechSkills,
-		skillMap,
-		setSkillMap,
-	} = useTechSkillContext();
+	const { fullList, skillMap, setSkillMap } = useTechSkillContext();
 
-	/** the current position(1-5) the user is at to select */
-	const [currentPosition, setCurrentPosition] =
-		React.useState<SkillPosition>(1);
+	const [state, dispatch] = React.useReducer(
+		skillReducer,
+		fullList,
+		initialSkillState
+	);
 
 	const handleCloseModal = () => {
 		onClose();
-		setCurrentPosition(1);
-		setUnselectedTechSkills(tech);
+		dispatch({ type: 'RESET', payload: fullList });
+		// ^resets, todo: change if u wanna persist.. maybe instead of calling reste it calls to save in local..
 		setSkillMap({
 			...freshSkillMap,
 		});
 	};
 
-	// TODO: change state logic to a reducer (https://react.dev/learn/extracting-state-logic-into-a-reducer)
-
 	const handleAddSelectedSkill = (skill: Skill) => {
-		const newUnselectedTechSkills = unselectedTechSkills.filter(
-			(s) => s.id !== skill.id
-		);
-		setUnselectedTechSkills(newUnselectedTechSkills);
+		if (isFull(skillMap)) {
+			return alert('You have already selected 5 skills');
+		}
+		dispatch({ type: 'ADD_SKILL', payload: skill });
 		const previousMapObject = { ...skillMap };
-		previousMapObject[currentPosition] = skill;
+		previousMapObject[state.currentPosition] = skill;
 		setSkillMap(previousMapObject);
-		if (currentPosition >= 5) return; // if at the last position, don't increment
-		setCurrentPosition(
-			(currentPosition) => (currentPosition + 1) as SkillPosition
-		);
 	};
 
 	const handleRemoveSelectedSkill = (skill: Skill, position: SkillPosition) => {
-		const newUnselectedTechSkills = [...unselectedTechSkills, skill];
-		// add the skill back into the unselected list
-		setUnselectedTechSkills(newUnselectedTechSkills);
+		const _isFull = isFull(skillMap);
+		dispatch({
+			type: 'REMOVE_SKILL',
+			payload: { skill, position, isFull: _isFull },
+		});
 		const newMapObject = { ...skillMap };
-		// shift all the skills up in the objectMap
 		for (let i = position; i < 5; i++) {
 			newMapObject[i] = newMapObject[(i + 1) as SkillPosition];
 		}
+		if (_isFull) {
+			newMapObject[5] = freshSkillMap[5];
+			setSkillMap(newMapObject);
+			return;
+		}
 		setSkillMap(newMapObject);
-		setCurrentPosition(
-			(currentPosition) => (currentPosition - 1) as SkillPosition
-		);
 	};
 
 	if (!show) return <></>;
@@ -86,15 +134,15 @@ export const TechSkillModal: React.FC<TechSkillModalProps> = ({
 				</IconButton>
 			</div>
 			<div className="modal-content">
-				{Object.entries(skillMap).map(([key, skill], index) => {
+				{Object.entries(skillMap).map(([key, skill]) => {
 					if (!skill || !skill.id)
 						return (
 							<InputItem
 								key={`${key}-empty`}
-								options={unselectedTechSkills}
+								options={state.unselectedTechSkills}
 								onSelectionChange={handleAddSelectedSkill}
 								position={+key as SkillPosition}
-								isDisabled={+key > currentPosition}
+								isDisabled={+key > state.currentPosition}
 							/>
 						);
 					return (
@@ -113,10 +161,15 @@ export const TechSkillModal: React.FC<TechSkillModalProps> = ({
 				})}
 			</div>
 			<div className="modal-sidebar">
-				<h4>Suggested Skills</h4>
-				{unselectedTechSkills.map((s) => (
-					<div key={s.name} className="skill-item">
-						+<span>{s.name}</span>
+				<h4 className="modal-sidebar-title">Suggested Skills</h4>
+				{state.unselectedTechSkills.map((s) => (
+					<div
+						key={s.name}
+						className="skill-item"
+						title={`add ${s.name} skill`}
+						onClick={() => handleAddSelectedSkill(s)}
+					>
+						<span className="skill-item-title">+&nbsp;{s.name}</span>
 					</div>
 				))}
 			</div>
